48d9df102852634babfd1fad083df9d0fe04f8e1
Marjona <136147628+Coralvsoo@users.noreply.github.com>
2025-04-24 09:59:29 +0500

    Add files via upload

---
+++ to_do_list/main.js
@@ -0,0 +1,14 @@
+const box = document.createElement('div');
+const body = document.getElementById('body');
+
+body.appendChild('box')
+box.style.border ='solid';
+box.style.backgroundColor ='red'
+box.style.width ='50px';
+box.style.height ='100vh';
+
+const h1 = document.getElementsByTagName('h1');
+h1.style.color = 'red'
+
+
+const hi = prompt('hello');
